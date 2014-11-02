63d20ef79a6fc21b42b3a0855f2e623d15836aad
DareJava <fatimehindare@gmail.com>
2014-11-02 16:17:24 +0100

    ist android commit

---
+++ kpoll-android/src/com/karmelos/kpoll/android/RespondActivity.java
@@ -0,0 +1,15 @@
+package com.karmelos.kpoll.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RespondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_respond);
+	}
+}
